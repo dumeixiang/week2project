78f0d95d137da26d8389abd7a045401cce4ace03
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-07 19:04:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import pandas as pd
 def mycar(data):
   mydataset = {'cars': ["BMW", "Volvo", "Ford"],'counts': [3, 7, 2]}
   mycar1 = pd.DataFrame(mydataset)
-  mycar1 = mycar.max()
+  mycar1 = mycar1.max()
   car_column = mycar1['counts']
   if data == car_column :
     return True
