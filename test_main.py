e5a2fd27b35ea810552fef01d36c51c92104343a
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-12 19:36:53 -0400

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,9 +6,10 @@ from main import mycar
 
 def test_data():
     # Test with dataset
-    result = mycar(7)
-    assert result, "Test failed to meet the dataset"
-
+    data= pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+    result = development(data)
+    assert result == 47.790116494845364
 
 if __name__ == "__main__":
     test_data()
