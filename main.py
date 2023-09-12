32239895b7d092cf8304457fd7c9e7a4950d073b
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-12 19:08:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,26 @@
 Main code
 """
 import pandas as pd
+import numpy as np
 
-def mycar(data):
-    mydataset = {'cars': ["BMW", "Volvo", "Ford"],'counts': [3, 7, 2]}
-    mycar1 = pd.DataFrame(mydataset)
-    mycar1 = mycar1.max()
-    car_column = mycar1['counts']
-    if data == car_column :
-        return True
-    else:
-        return False
+# Download World Development Indicators
+def development(data)
+    wdi= pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
+)
+    wdi["Adolescent fertility rate (births per 1,000 women ages 15-19)"].describe()
+return wdi["Adolescent fertility rate (births per 1,000 women ages 15-19)"].describe().loc['mean']
+
+# generate Plot
+
+import seaborn.objects as so
+import seaborn as sns
+from matplotlib import style
+
+def plot():
+    my_chart = (
+    so.Plot(wdi, x="Mortality rate, infant (per 1,000 live births)", y="GDP per capita (constant 2010 US$)")
+    .add(so.Line(), so.PolyFit(order=2))
+    .add(so.Dot())
+    .label(title="Log GDP and infat Mortality")
+    .theme({**style.library["seaborn-whitegrid"]}))
+    my_chart.show()
