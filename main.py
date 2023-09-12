a793cd0420a70453cc1d4308969765b3fe654f78
dumeixiang <141801043+dumeixiang@users.noreply.github.com>
2023-09-12 19:29:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ Main code
 import pandas as pd
 import numpy as np
 
+
 # Download World Development Indicators
-def development(data)
-    wdi= pd.read_csv("https://media.githubusercontent.com/media/nickeubank/MIDS_Data/master/World_Development_Indicators/wdi_small_tidy_2015.csv"
-)
-    wdi["Adolescent fertility rate (births per 1,000 women ages 15-19)"].describe()
-return wdi["Adolescent fertility rate (births per 1,000 women ages 15-19)"].describe().loc['mean']
+def development(data): 
+    return data["Adolescent fertility rate (births per 1,000 women ages 15-19)"].describe().
 
 # generate Plot
 
@@ -17,9 +15,8 @@ import seaborn.objects as so
 import seaborn as sns
 from matplotlib import style
 
-def plot():
-    my_chart = (
-    so.Plot(wdi, x="Mortality rate, infant (per 1,000 live births)", y="GDP per capita (constant 2010 US$)")
+def plot(data):
+    my_chart = (so.Plot(data, x="Mortality rate, infant (per 1,000 live births)", y="GDP per capita (constant 2010 US$)")
     .add(so.Line(), so.PolyFit(order=2))
     .add(so.Dot())
     .label(title="Log GDP and infat Mortality")
